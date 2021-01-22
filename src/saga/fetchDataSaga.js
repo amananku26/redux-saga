07307de8e876ca7f08f08851abcf5835ab20677c
AMAN ANKU <amankumaranku@gmail.com>
2021-01-22 23:48:15 +0530

    update

--- src/saga/fetchDataSaga.js
+++ src/saga/fetchDataSaga.js
@@ -1,8 +1,9 @@
-import { call, takeEvery } from "redux-saga/effects";
+import { call,delay, takeEvery } from "redux-saga/effects";
 import {types} from "../redux/types"
 import axios from "axios"
 
 function* asyncFetchRequest(action){
+    yield  delay(4000)//make delay by 4s to retrive the data
   try{
       const url = `https://reqres.in/api/users/${action.payload}`
       const response  = yield call(()=> axios.get(url))
@@ -14,6 +15,7 @@ function* asyncFetchRequest(action){
 }
 
 export function* watchFetchdata(){
+    
    yield takeEvery(types.SEND_REQUEST , asyncFetchRequest)
 
 }
