4a01c1a27a6b430203029daafd36a6abdc6b024a
AMAN ANKU <amankumaranku@gmail.com>
2021-01-27 09:43:26 +0530

    update redux store using saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { createStore,applyMiddleware,combineReducers } from "redux";
 import createSagaMiddleware  from "redux-saga"
 import FetchDataReducer from './reducer' // if we do not want to use any extra file for rootReducer
 import RootSaga from "../saga/RootSaga"
-// import RootReducer from "./RootReducer"
+// import RootReducer from "./RootReducer" // if we  want to use  extra file for rootReducer remove this from comment
 
 const RootReducer = combineReducers({// if we do not want to use any extra file for rootReducer
     FetchDataReducer// if we do not want to use any extra file for rootReducer
